c9601f71e0d43a09fd222c2869668a94927e6d1d
chottedifs <husadif.aldi@gmail.com>
2022-08-24 00:00:52 +0700

    Update API Tagihan

--- app/Http/Controllers/API/TagihanController.php
+++ app/Http/Controllers/API/TagihanController.php
@@ -11,29 +11,37 @@ use Auth;
 class TagihanController extends Controller
 {
     public function all(Request $request){
-        $tagihan = Tagihan::with('SewaKios','MasterStatus')->where('user_id', Auth::user()->User->id)->get();
+        $tahun = $request->input('tahun');
 
-        foreach ($tagihan as $dataTagihan){
-            $response[] = [
-                'id_sewa' => $dataTagihan->SewaKios->id,
-                'id_tagihan' => $dataTagihan->id,
-                'kode_tagihan' => $dataTagihan->kode_tagihan,
-                'nama_penyewa' => $dataTagihan->User->nama_lengkap,
-                'no_rekening' => $dataTagihan->User->rekening,
-                'nama_kios' => $dataTagihan->SewaKios->RelasiKios->Kios->nama_kios,
-                'lokasi_kios' => $dataTagihan->SewaKios->RelasiKios->Lokasi->nama_lokasi,
-                'periode' => date('m-Y', strtotime($dataTagihan->periode)),
-                'tagihan_kios' => $dataTagihan->tagihan_kios,
-                'kwh' => $dataTagihan->total_kwh,
-                'tagihan_kwh' => $dataTagihan->tagihan_kwh,
-                'total_tagihan' => $dataTagihan->tagihan_kwh + $dataTagihan->tagihan_kios,
-                'status_bayar' => $dataTagihan->MasterStatus->nama_status
-            ];
-        };
+        if($tahun) {
+            $tagihan = Tagihan::with('SewaKios','MasterStatus')
+                        ->where('user_id', Auth::user()->User->id)
+                        ->whereYear('periode', $tahun)
+                        ->get();
 
-        if($response)
-            return ResponseFormatter::success($response, 'Data Sewa Kios Berhasil di Ambil');
-        else
-            return ResponseFormatter::error(null, 'Data Sewa Kios Tidak Ada', 404);
+            foreach ($tagihan as $dataTagihan){
+                $response [] = [
+                    'id_sewa' =>$dataTagihan->SewaKios->id,
+                    'id_tagihan' =>$dataTagihan ->id,
+                    'kode_tagihan' =>$dataTagihan->kode_tagihan,
+                    'nama_penyewa' =>$dataTagihan->User->nama_lengkap,
+                    'no_rekening' =>$dataTagihan->User->rekening,
+                    'nama_kios' =>$dataTagihan->SewaKios->RelasiKios->Kios->nama_kios,
+                    'lokasi_kios' => $dataTagihan->SewaKios->RelasiKios->Lokasi->nama_lokasi,
+                    'periode' => date('m-Y', strtotime($dataTagihan->periode)),
+                    'tagihan_kios' => $dataTagihan->tagihan_kios,
+                    'kwh' => $dataTagihan->total_kwh,
+                    'tagihan_kwh' => $dataTagihan->tagihan_kwh,
+                    'total_tagihan' => $dataTagihan->tagihan_kwh + $dataTagihan->tagihan_kios,
+                    'status_bayar' => $dataTagihan->MasterStatus->nama_status
+                ];
+            };
+
+            if($tagihan->isEmpty())
+                return ResponseFormatter::error(null, 'Data Tagihan Tidak Ada', 404);
+            else
+                return ResponseFormatter::success($response, 'Data Tagihan Berhasil di Ambil');
+        }
+
     }
 }
